e098698292891a560d960a650dc2fb120f28f990
Markus Pfundstein <markus.pfundstein@student.uva.nl>
2016-03-03 12:15:51 +0100

    forgot func

---
+++ src/func.lisp
@@ -0,0 +1,17 @@
+(defpackage #:func
+  (:use #:cl #:iterate)
+  (:export
+   #:cartesian2
+   #:iter-cartesian2))
+
+(in-package #:func)
+
+(defmacro cartesian2 (seq1 seq2)
+  `(reduce 'append
+	   (mapcar #'(lambda (e1)
+		       (mapcar #'(lambda (e2) (list e1 e2)) ,seq2))
+		   ,seq1)))
+
+(defmacro iter-cartesian2 (((e1 e2) seq1 seq2) cmd)
+  `(iter (for (,e1 ,e2) in (cartesian2 ,seq1 ,seq2))
+	 ,cmd))
